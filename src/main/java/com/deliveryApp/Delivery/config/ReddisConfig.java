f81960f709ec697791636182d020088e9503058e
uriel <urielch997@gmail.com>
2023-01-03 11:59:57 -0800

    Se  realizo cambio en  clase de redis

--- src/main/java/com/deliveryApp/Delivery/config/ReddisConfig.java
+++ src/main/java/com/deliveryApp/Delivery/config/ReddisConfig.java
@@ -1,7 +1,9 @@
 package com.deliveryApp.Delivery.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -11,19 +13,31 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories(basePackages = "com.deliveryApp.Delivery.reddis")
 public class ReddisConfig {
+	
+	@Value("${spring.redis.username}")
+	private String username;
+	
+	@Value("${spring.redis.password}")
+	private String password;
+	
+	@Value("${spring.redis.url}")
+	private String url;
+	
+	@Value("${spring.redis.port}")
+	private  Integer  port;
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		 JedisConnectionFactory jedisConFactory
-	      = new JedisConnectionFactory();
-	    return jedisConFactory;
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(url, port);
+		redisStandaloneConfiguration.setUsername(username);
+		redisStandaloneConfiguration.setPassword(password);
+		return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
-	RedisTemplate<byte[], byte[]> redisTemplate() {
+ RedisTemplate<byte[], byte[]> redisTemplate() {
 	    RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
 	    Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-	    template.setConnectionFactory(jedisConnectionFactory());
 	    template.setConnectionFactory(jedisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
